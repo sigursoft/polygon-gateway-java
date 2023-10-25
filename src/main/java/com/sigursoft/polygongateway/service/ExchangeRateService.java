783c508486b52e14ddfa26c0eaee3607c77f8d3f
Anton Kozik <pazitron@gmail.com>
2023-10-25 09:35:58 +0200

    code cleanup

--- src/main/java/com/sigursoft/polygongateway/service/ExchangeRateService.java
+++ src/main/java/com/sigursoft/polygongateway/service/ExchangeRateService.java
@@ -2,7 +2,6 @@ package com.sigursoft.polygongateway.service;
 
 import com.sigursoft.polygongateway.components.PolygonWebClient;
 import com.sigursoft.polygongateway.domain.ExchangeRate;
-import com.sigursoft.polygongateway.domain.PolygonForexResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -18,9 +17,9 @@ public class ExchangeRateService {
 	}
 
 	public Mono<ExchangeRate> provideExchangeRate(String buyCurrency, String sellCurrency) {
-		Mono<PolygonForexResponse> polygonRate = polygonWebClient.fetchPreviousDayExchangeRate(buyCurrency,
-				sellCurrency);
-		return polygonRate.flatMap(polygonForexResponse -> Mono.just(polygonForexResponse.results().get(0).vw()))
+		var polygonRate = polygonWebClient.fetchPreviousDayExchangeRate(buyCurrency, sellCurrency);
+		return polygonRate
+                .flatMap(polygonForexResponse -> Mono.just(polygonForexResponse.results().get(0).vw()))
 				.flatMap(rate -> Mono.just(new ExchangeRate(buyCurrency, sellCurrency, rate)));
 	}
 
