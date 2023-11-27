1c253ebceafefe457034f6e2b8a5f6b99ec25f1e
Anton Kozik <pazitron@gmail.com>
2023-11-27 18:17:49 +0100

    use WireMock with Testcontainers

--- src/test/java/com/sigursoft/polygongateway/PolygonGatewayIntegrationTests.java
+++ src/test/java/com/sigursoft/polygongateway/PolygonGatewayIntegrationTests.java
@@ -1,34 +1,39 @@
 package com.sigursoft.polygongateway;
 
-import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import com.sigursoft.polygongateway.domain.ExchangeRate;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.wiremock.integrations.testcontainers.WireMockContainer;
 
 import java.math.BigDecimal;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
-
-@Disabled
-@WireMockTest(httpPort = 8099)
-@SpringBootTest(webEnvironment = RANDOM_PORT, properties = { "polygon.baseUrl=http://localhost:8099" })
+@Testcontainers
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class PolygonGatewayIntegrationTests {
+
+    @Container
+    static WireMockContainer wiremockServer = new WireMockContainer("wiremock/wiremock:3.3.1-1")
+            .withMappingFromResource("fx", PolygonGatewayIntegrationTests.class, "mapping.json");
+
+    @DynamicPropertySource
+    static void props(DynamicPropertyRegistry registry) {
+        registry.add("polygon.baseUrl", () -> "http://localhost:" + wiremockServer.getPort());
+    }
 
     @Test
     @DisplayName("should return correct exchange rate from polygon mock")
     public void testSuccessfullyReturnExchangeRateFromPolygon(@Autowired WebTestClient webClient) {
         // arrange
         var expectedResponse = new ExchangeRate("USD", "EUR", new BigDecimal("0.9407"));
-        stubFor(get(urlEqualTo("/v2/aggs/ticker/C:USDEUR/prev"))
-                .willReturn(aResponse()
-                        .withHeader("Content-Type", "application/json")
-                        .withBodyFile("polygon-response.json")));
         // act & assert
         webClient.get()
                 .uri("/exchange-rate/USD/EUR")
